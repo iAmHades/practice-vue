3e33820aed5f1d65886f82cf1175c3a175fa9084
iAmHades <hi@iamhades.com>
2017-08-01 23:30:01 +0800

    修复渲染函数中 变量未解析的问题

--- src/compile/index.js
+++ src/compile/index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 import parse from './html-prase.js'
+import textParser from './text-parser.js'
 
 const cache = Object.create(null)
 
@@ -12,20 +13,20 @@ export default function compile(html) {
 
 function renderFunction(ast) {
 	let attrs, child, render
-	// 遍历到最底层
-	if (ast.children && ast.children.length===1 && typeof(ast.children[0])==='string') {
+		// 遍历到最底层
+	if (ast.children && ast.children.length === 1 && typeof(ast.children[0]) === 'string') {
 		if (ast.attrs.length > 0) {
-    		return 'h("'+ast.tag+'", {attrs: '+ getAttrsStr(ast.attrsMap) +'}, "'+ast.children[0]+'")'
-		}else{
-			return 'h("'+ast.tag+'", "'+ast.children[0]+'")'
+			return 'h("' + ast.tag + '", {attrs: ' + getAttrsStr(ast.attrsMap) + '}, ' + generateText(ast.children[0]) + ')'
+		} else {
+			return 'h("' + ast.tag + '", ' + generateText(ast.children[0]) + ')'
 		}
-	} else if(ast.children) {
+	} else if (ast.children) {
 		child = getChildRender(ast.children)
 	}
 	if (ast.attrs.length > 0) {
-		render = 'h("'+ast.tag+'", {attrs: '+ getAttrsStr(ast.attrsMap) +'}, ['+child+'])'
-	}else{
-        render = 'h("'+ast.tag+'", ['+child+'])'
+		render = 'h("' + ast.tag + '", {attrs: ' + getAttrsStr(ast.attrsMap) + '}, [' + child + '])'
+	} else {
+		render = 'h("' + ast.tag + '", [' + child + '])'
 	}
 	return render
 }
@@ -38,18 +39,26 @@ function getChildRender(childs) {
 			else return true
 		})
 		return childs.map((child) => {
-    			return renderFunction(child)
-		       })
+			return renderFunction(child)
+		})
 	}
 }
 
-function getAttrsStr(obj){
+function getAttrsStr(obj) {
 	let str = []
-	Object.keys(obj).forEach((key)=> {
-		str.push(key + ':"' + obj[key]+'"')
+	Object.keys(obj).forEach((key) => {
+		str.push(key + ':"' + obj[key] + '"')
 	})
-	return '{'+ str.join(',')+'}'
+	return '{' + str.join(',') + '}'
 }
 
+function generateText(text) {
+	const exp = textParser(text)
+	return 'String(' + escapeNewlines(exp) + ')'
+}
+
+function escapeNewlines (str) {
+  return str.replace(/\n/g, '\\n')
+}
 
 function h() {}
